f7504da4acfd00bd72bf33bc8f81aff8a40c93b7
Nicolás Raposo <nicolasraposo11@gmail.com>
2025-05-23 22:41:44 -0300

    Implemento Maquina de estados para led con botones
    Implemento una maquina de estados que toma cada 1ms la medida del touchpad y detecta si algún botón fue presionado, por ahora solo prende y apaga el led

--- main/main.c
+++ main/main.c
@@ -3,51 +3,85 @@
 #include "mi_delay.h"
 #include "driver/touch_pad.h"
 
-
+void get_state();
+#define TOUCH_THRESHOLD 19000
 #define TOUCH_BUTTON_NUM 6
+int button_pressed;
+char STATE;
+led_strip_t *strip;
 
 static const touch_pad_t buttons[TOUCH_BUTTON_NUM] = {
-    TOUCH_PAD_NUM1,   // TP5 - Vol Up
-    TOUCH_PAD_NUM2,   // TP2 - Play/Pause
-    TOUCH_PAD_NUM3,   // TP6 - Vol Down
-    TOUCH_PAD_NUM5,   // TP4 - Record
-    TOUCH_PAD_NUM6,   // TP1 - Photo
-    TOUCH_PAD_NUM11,  // TP3 - Network
+    TOUCH_PAD_NUM1,   // TP5 - VOL_UP
+    TOUCH_PAD_NUM2,   // TP2 - PLAY/PAUSE
+    TOUCH_PAD_NUM3,   // TP6 - VOL_DOWN
+    TOUCH_PAD_NUM5,   // TP4 - RECORD
+    TOUCH_PAD_NUM6,   // TP1 - PHOTO
+    TOUCH_PAD_NUM11,  // TP3 - NETWORK
 };
 
-void testbench_miled(void);
-
 void app_main(void)
-{
-    touch_pad_init();
+{   
+    //Iniciamos el led
+    led_rgb_init(&strip);
 
+    // Iniciamos el touchpad
+    touch_pad_init(); 
     for (int i = 0; i < TOUCH_BUTTON_NUM; i++) {
+        // Configuramos todos los botones
         touch_pad_config(buttons[i]);
     }
+    touch_pad_set_fsm_mode(TOUCH_FSM_MODE_SW);
 
-    // Configuración opcional de denoise
-    touch_pad_denoise_t denoise = {
-        .grade = TOUCH_PAD_DENOISE_BIT4,
-        .cap_level = TOUCH_PAD_DENOISE_CAP_L4,
-    };
-    touch_pad_denoise_set_config(&denoise);
-    touch_pad_denoise_enable();
-
-    touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
-    touch_pad_fsm_start();
-
-    printf("Iniciando lectura de botones táctiles:\n");
-
-    while (1) {
-        uint32_t value;
-        for (int i = 0; i < TOUCH_BUTTON_NUM; i++) {
-            touch_pad_read_raw_data(buttons[i], &value);
-            printf("T%d: [%4"PRIu32"] ", buttons[i], value);
+    while(1){
+        get_state();
+        
+        switch (STATE)
+        {
+            case 'A':
+                turn_led_on(strip, 255, 0, 0);
+                break;
+            case 'B':
+                turn_led_off(strip);
+                break;
+            case 'X':
+                turn_led_on(strip, 255, 255, 255);
+                break;
+            default:
+                turn_led_off(strip);
+                break;
         }
-        printf("\n");
+        
+    }
+}
 
-        // Espera sin usar FreeRTOS
-        delay(50,'m'); 
+void get_state(void){
+
+    uint32_t value;
+
+    for (int i = 0; i < TOUCH_BUTTON_NUM; i++) {
+        touch_pad_sw_start();
+        delay(10,'m');
+        touch_pad_read_raw_data(buttons[i], &value);
+        printf("T%d: [%4"PRIu32"]\n", buttons[i], value);
+            if (value > TOUCH_THRESHOLD){
+              button_pressed = i;
+              switch (button_pressed)
+              {
+                case 0:
+                    STATE = 'A';
+                    printf("Boton VOL_UP presionado, STATE A\n");
+                    
+                    return;
+                case 2:
+                    STATE = 'B';
+                    printf("Boton VOL_DOWN presionado, STATE B\n");
+                    return;
+                default:
+                    STATE = 'X';
+                    printf("Boton %d presionado, parece no hacer nada\n", button_pressed);
+                    return;
+            }
+        }
     }
 }
 
